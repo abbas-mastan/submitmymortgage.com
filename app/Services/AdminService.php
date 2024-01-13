25936682c80f69a7d99d65164783501db8fc4535
abbas mastan <abbasmastan2424@gmail.com>
2024-01-13 09:58:09 +0500

    users media filtered

--- app/Services/AdminService.php
+++ app/Services/AdminService.php
@@ -224,7 +224,7 @@ class AdminService
         }
         $filesIds = [];
         foreach ($users as $user) {
-            if($user->media){
+            if($user->media != null){
                 $media = $user->media;
                 foreach ($media as $m) {
                     $filesIds[] = $m->id;
