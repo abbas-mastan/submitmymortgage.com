e3770d074c686c1551875d69039d0fee42509366
abbas mastan <abbasmastan2424@gmail.com>
2024-01-29 12:43:15 +0500

    docx preview changed to msoffice

--- resources/views/show-docx.blade.php
+++ resources/views/show-docx.blade.php
@@ -1,2 +1,3 @@
 
-    <iframe width="100%" height="100%" src="https://docs.google.com/gview?url={{$path}}&embedded=true"></iframe>
+    {{-- <iframe width="100%" height="100%" src="https://docs.google.com/gview?url={{$path}}&embedded=true"></iframe> --}}
+    <iframe width="100%" height="100%" src="https://view.officeapps.live.com/op/embed.aspx?src={{$path}}&embedded=true"></iframe>
