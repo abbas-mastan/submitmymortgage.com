43483972f5962143981b25822998b78900b18dfa
abbas mastan <abbasmastan2424@gmail.com>
2024-01-31 15:47:38 +0500

    notifications layout changed

--- resources/views/admin/dashboard/menu.blade.php
+++ resources/views/admin/dashboard/menu.blade.php
@@ -85,7 +85,6 @@
                 @foreach (Auth::user()->notifications as $message)
                     @php
                     if($message->data['project'] ?? false){
-                        dd($message->data['project']);
                         $project = \App\Models\Project::find($message->data['project']);   
                     }else{
                         $project = \App\Models\Project::where('borrower_id', $message->data['user_id'])->first();   
