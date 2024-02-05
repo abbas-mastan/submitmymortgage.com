4ff7ef038516c0fc154bd039f209e2d7de3bb574
abbas mastan <abbasmastan2424@gmail.com>
2024-02-05 15:05:24 +0500

    email styles completed

--- resources/views/set-password.blade.php
+++ resources/views/set-password.blade.php
@@ -1,29 +1,45 @@
-@extends('layouts.empty')
-@section('content')
-    <div class="mt-40 m-10 w-[50%] bg-['#edf2f7'] flex items-center justify-center">
-        <div>
-            <h2 class="flex justify-center my-3 ">
-                <span>
-                    SubmitMyMortgage
-                </span>
-            </h2>
-            <div class="h-[50%] child w-100 flex-col justify-center mt-5 shadow-2xl bg-white p-10">
-                <h3 class="text-lg">Hello!</h3>
-                <p class="text-md mt-2">You are receiving this email to complete your registration process by entering your
-                    password.
-                </p>
-                <div class="flex justify-center">
-                    <a href="" class="text-center my-5 bg-black rounded text-white px-3 py-1.5">Create Password</a>
-                </div>
-                <p class="text-md">Regards,<br>
-                    SubmitMyMortgage</p>
-
-                <hr class="my-3">
-                <p class="text-sm text-gray-400">
-                    If you're having trouble clicking the "Choose Password" button, copy and paste the URL below into your
-                    web browse:  
-                </p>
-            </div>
-        </div>
-    </div>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Register Email</title>
+</head>
+<body style="background-color: rgb(250, 242, 242);">
+<div style="display: flex;
+justify-content: center;
+align-items: center;">
+<div style="display: inline-flex;
+flex-direction: column;
+align-items: center;">
+<h2>
+<span>
+SubmitMyMortgage
+</span>
+</h2>
+<div style="height: 50%; box-shadow: 0 25px 50px -12px rgba(0, 0, 0, .25) !important; padding:3rem"
+class="child w-100 flex-col justify-center mt-5 bg-white p-10">
+<h3 style="font-size: 1.125rem; line-height: 1.75rem;" class="text-lg">Hello!</h3>
+<p style="font-size: 1rem; margin-top: 0.5rem;" class="text-md mt-2">
+You are receiving this email to complete your registration process by entering your password.
+</p>
+<div style="text-align: center;
+margin-top: 1.5rem;
+margin-bottom: 1.5rem;">
+<a href="#"
+style="text-decoration:none; background-color: black; border-radius: 0.25rem; color: white; padding-left: 0.75rem; 
+padding-right: 0.75rem; padding-bottom: 0.375rem; padding-top: 0.375rem;"
+class="text-center my-5 bg-black rounded text-white px-3 py-1.5">
+Create Password
+</a>
+</div>
+<p style="font-size: 1rem;" class="text-md">
+Regards,<br> SubmitMyMortgage
+</p>
+<hr style="margin-top: 0.5rem;" class="my-3">
+</div>
+</div>
+</div>
+</body>
+</html>
--- resources/views/vendor/notifications/set-password.blade.php
+++ resources/views/vendor/notifications/set-password.blade.php
@@ -1,25 +1,39 @@
-@extends('layouts.empty')
-@section('content')
-<div class="mt-40 m-10 w-[50%] bg-['#edf2f7'] flex items-center justify-center">
-<div>
-<h2 class="flex justify-center my-3 ">
-<span>
-SubmitMyMortgage
-</span>
-</h2>
-<div class="h-[50%] child w-100 flex-col justify-center mt-5 shadow-2xl bg-white p-10">
-<h3 class="text-lg">Hello!</h3>
-<p class="text-md mt-2">You are receiving this email to complete your registration process by entering your
-password.
-</p>
-<div class="flex justify-center">
-<a href="{{ $url }}" class="text-center my-5 bg-black rounded text-white px-3 py-1.5">Create
-Password</a>
-</div>
-<p class="text-md">Regards,<br>
-SubmitMyMortgage</p>
-<hr class="my-3">
-</div>
-</div>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Email</title>
+</head>
+<body style="background-color: #edf2f7;">
+    <div style="padding: 50px">
+        <div style="text-align: center;">
+            <h2>
+                <span style="color:black;">
+                    SubmitMyMortgage
+                </span>
+            </h2>
+            <div
+                style="height: 50%; background-color:white; box-shadow: 0 25px 50px -12px rgba(0, 0, 0, .25); padding:3rem; margin-bottom:20px;">
+                <h3 style="font-size: 1.125rem; line-height: 1.75rem;text-align:left; color:black;" class="text-lg">
+                    Hello!</h3>
+                <p style="color:black; font-size: 1rem; margin-top: 0.5rem; text-align:left;">
+                    You are receiving this email to complete your registration process by entering your password.
+                </p>
+                <div style="text-align: center; margin-top: 1.5rem; margin-bottom: 1.5rem;">
+                    <a href="{{ $url }}"
+                        style="text-decoration:none; background-color: black; border-radius: 0.25rem; color: white; padding:0.5rem 0.90rem;">
+                        Create Password
+                    </a>
+                </div>
+                <p style="color:black;text-align:left;font-size: 1rem;" class="text-md">
+                    Regards,<br> SubmitMyMortgage
+                </p>
+                <hr style="margin-top: 0.5rem;" class="my-3">
+            </div>
+            <small style="color:black">© {{ date('Y') }} SubmitMyMortgage. All rights reserved.</small>
+        </div>
+    </div>
+</body>
+</html>
