d8e5a8bdc33d102286608f2e63ba9f2e3fdf1f15
abbas mastan <abbasmastan2424@gmail.com>
2024-02-24 11:53:48 +0500

    email disabled removed

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.empty')
 @section('content')
-    <div class="mx-auto w-1/3 mt-40">
+    <div class="mx-auto w-1/3 flex flex-col justify-center items-center">
 
         <div class="">
             <h1 class="xl:text-3xl text-2xl uppercase text-center font-bold text-white">
@@ -13,7 +13,7 @@
             </h1>
         </div>
         <form action="{{ url(\Request::route()->getName() === 'user.register' ? 'set-password-new-user' : '/reset-password') }}"
-            method="post" class=" w-7/8">
+            method="post" class="text-center w-full">
             @include('parts.alerts')
             @csrf
             <input value="{{ $token }}" type="hidden" name="token">
@@ -22,7 +22,7 @@
                     <label for="username" class="text-white text-md xl:text-xl opacity-70">Email</label>
                 </div>
                 <div class="mt-2">
-                    <input type="email" value="{{ old('email',$user->email ?? null) }}" {{ isset($user) && $user->email ? 'disabled' : ''}}
+                    <input type="email" value="{{ old('email',$user->email ?? null) }}"
                         class="rounded-md py-2 w-full focus:outline-none focus:border-none  focus:ring-1 focus:ring-blue-400"
                         name="email" id="username" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Email">
                 </div>
@@ -48,12 +48,8 @@
                         name="password_confirmation" id="password" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;********">
                 </div>
             </div>
-            <div class="mt-5 grid grid-cols-6">
-                <div class="col-span-2 text-right mr-12">
-                    &nbsp;
-                </div>
+            <div class="mt-5">
 
-                <div class="col-span-4 ml-1 ">
                     <button type="submit"
                         class=" opacity-70  border-2 border-white rounded-md bg-white px-10 py-2  focus:outline-none focus:border-none  focus:ring-1 focus:ring-blue-400">
                         @if (\Request::route()->getName() === 'user.register')
@@ -63,7 +59,6 @@
                         @endif
                         Password
                     </button>
-                </div>
 
             </div>
 
